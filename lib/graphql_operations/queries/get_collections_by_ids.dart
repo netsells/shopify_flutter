adaad8769120062d4381f1e57039e5475e03cd8a
Oli Warriner <owarriner92@gmail.com>
2022-05-30 09:13:25 +0100

    update collections by id query

--- lib/graphql_operations/queries/get_collections_by_ids.dart
+++ lib/graphql_operations/queries/get_collections_by_ids.dart
@@ -13,6 +13,93 @@ query getCollectionsByIds($ids: [ID!]!){
       }
       title
       updatedAt
+      products(first: 1000, sortKey: $sortKey, after: $cursor) {
+        edges {
+          cursor
+          node {
+          options(first: 50) {
+            id
+            name
+            values
+            } 
+            availableForSale
+            collections(first: 250) {
+              edges {
+                node {
+                  description
+                  descriptionHtml
+                  handle
+                  id
+                  title
+                  updatedAt
+                }
+              }
+            }
+            createdAt
+            description
+            descriptionHtml
+            handle
+            id
+            images(first: 250) {
+              edges {
+                node {
+                  altText
+                  id
+                  originalSrc
+                }
+              }
+            }
+            onlineStoreUrl
+            productType
+            publishedAt
+            tags
+            title
+            updatedAt
+            vendor
+            variants(first: 250) {
+              edges {
+                node {
+                  title
+                  image {
+                    altText
+                    id
+                    originalSrc
+                  }
+                  priceV2 {
+                    amount
+                    currencyCode
+                  }
+                  compareAtPriceV2 {
+                    amount
+                    currencyCode
+                  }
+                  weight
+                  weightUnit
+                  sku
+                  requiresShipping
+                  selectedOptions {
+                    name
+                    value
+                  }
+                  availableForSale
+                  id
+                  quantityAvailable
+                  unitPrice {
+                      amount
+                      currencyCode
+                  }
+                  unitPriceMeasurement {
+                      measuredType
+                      quantityUnit
+                      quantityValue
+                      referenceUnit
+                      referenceValue
+                  }
+                }
+              }
+            }
+          }
+        }
     }
   }
 }''';
