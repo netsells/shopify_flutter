c2cce7547a7591170d1a59df4fa560f4fb373299
benbaggley <ben.baggley@netsells.co.uk>
2022-06-08 13:26:39 +0100

    Added products_and_collections export

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -2,6 +2,7 @@ export 'src/checkout/checkout.dart';
 export 'src/collection/collection.dart';
 export 'src/order/order.dart';
 export 'src/product/product.dart';
+export 'src/product/products_and_collections.dart';
 export 'src/shopify_user/shopify_user.dart';
 export 'src/article/article.dart';
 export 'src/blog/blog.dart';
