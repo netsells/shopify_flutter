03904c566ae625d1f1567b595358e1872244045b
Helena Sewell <helena.sewell@netsells.co.uk>
2022-07-12 15:43:38 +0100

    check the correct error token

--- lib/shopify/src/shopify_auth.dart
+++ lib/shopify/src/shopify_auth.dart
@@ -151,7 +151,7 @@ class ShopifyAuth with ShopifyError {
         document: gql(customerAccessTokenCreate),
         variables: {'email': email, 'password': password});
     final QueryResult result = await _graphQLClient!.mutate(_options);
-    checkForError(result);
+    checkForError(result, errorKey: 'customerUserErrors');
     if (deleteThisPartOfCache) {
       _graphQLClient!.cache.writeQuery(_options.asRequest, data: {});
     }
